10d700bf2c60c1f6592e9a6cf6114ee298c4b0ed
ProdanSergey <prodan.sergey.od@gmail.com>
2021-11-13 13:22:55 +0200

    homework(13,14) DOM: Selectors, Attrbiutes

---
+++ 13 - DOM: Selectors, Traverse/app/index.js
@@ -0,0 +1,73 @@
+const APP = (() => {
+	return {
+		firstName() {
+			const MIN = 5;
+			const MAX = 20;
+
+			const validate = (input) => {
+				return input.length < MIN || input.length > MAX;
+			};
+
+			return STDIN.string(`Enter your first name: min length ${MIN}, max length ${MAX}`, validate);
+		},
+		lastName() {
+			const MIN = 5;
+			const MAX = 20;
+
+			const validate = (input) => {
+				return input.length < MIN || input.length > MAX;
+			};
+
+			return STDIN.string(`Enter your last name: min length ${MIN}, max length ${MAX}`, validate);
+		},
+		age() {
+			const MIN_YEAR = 1900;
+			const MAX_YEAR = new Date().getFullYear();
+
+			const validateYear = (input) => input < MIN_YEAR || input > MAX_YEAR; 
+
+			const year = STDIN.number(`Enter your year of birth: min ${MIN_YEAR}, max ${MAX_YEAR}`, validateYear);
+
+			const MIN_MONTH = 1;
+			const MAX_MONTH = 12;
+
+			const validateMonth = (input) => input < MIN_MONTH || input > MAX_MONTH; 
+
+			const month = STDIN.number(`Enter your month of month: min ${MIN_MONTH}, max ${MAX_MONTH}`, validateMonth);
+
+			const MIN_DAY = 1;
+			const MAX_DAY = DATE.getMaxDay(year, month);
+
+			const validateDay = (input) => input < MIN_DAY || input > MAX_DAY; 
+
+			const day = STDIN.number(`Enter your month of month: min ${MIN_DAY}, max ${MAX_DAY}`, validateDay);
+
+			const now = new Date();
+			const birth = new Date(year, month - 1, day);
+			const birthDay = new Date(now.getFullYear(), month - 1, day);
+
+			const fullAge = now.getFullYear() - birth.getFullYear();
+
+			if (now > birthDay) {
+				return fullAge;
+			}
+
+			return fullAge - 1;
+		},
+		date() {
+			const date = new Date();
+
+			return `${date.getDate()}/${date.getMonth()}/${date.getFullYear()} ${date.getHours()}:${date.getMinutes()}`;
+		}
+	};
+})();
+
+const spans = document.querySelectorAll('span');
+
+for (const span of spans) {
+	const key = span.innerText;
+
+	if (key in APP) {
+		span.innerText = APP[key]();
+	}
+}
