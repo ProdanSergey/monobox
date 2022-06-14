9912f546f41293056f03808b89084004408838fe
ProdanSergey <prodan.sergey.od@gmail.com>
2022-06-14 16:45:48 +0200

    rename project

--- src/webpack/React/src/components/button.styled.tsx
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-
-export const StyledButton = styled.button`
-  display: inline-flex;
-  padding: 6px;
-  width: fit-content;
-  cursor: pointer;
-  align-self: flex-end;
-`;
