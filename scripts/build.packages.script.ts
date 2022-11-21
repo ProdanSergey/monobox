450d034e98b5e81e6d49552d15d2c15ec79e819e
ProdanSergey <prodan.sergey.od@gmail.com>
2022-11-21 23:55:51 +0100

    [Chore] Automate package build script

--- scripts/build.packages.script.ts
+++ scripts/build.packages.script.ts
@@ -1,58 +1,88 @@
 import { promisify } from "node:util";
+import { readdir, readFile } from "node:fs/promises";
 import { exec } from "node:child_process";
 import dayjs from "dayjs";
 
-enum Targets {
-  "toolkit" = "toolkit",
-  "infra" = "infra",
-  "appointment-contract" = "appointment-contract",
-  "appointment-core" = "appointment-core",
-  "appointment-library" = "appointment-library",
-}
+type Target = string;
 
-type Target = keyof typeof Targets;
-
-const isTarget = (name?: string): name is Target => {
-  if (!name) {
-    return false;
-  }
-
-  if (Object.values(Targets).find((target) => Object.is(target, name))) {
-    return true;
-  }
-
-  console.error(`Package: ${name} can not be found.`);
-  process.exit(1);
+const isObject = <T extends Record<PropertyKey, unknown>>(target: unknown): target is T => {
+  return typeof target === "object" && !Array.isArray(target) && target !== null;
 };
 
 const diff = (timestamp: number): string => {
   return dayjs(performance.now()).diff(timestamp, "seconds", true).toFixed(3);
 };
 
-const build = async (name: Target) => {
-  const timestamp = performance.now();
-
-  try {
-    await promisify(exec)("yarn build", { cwd: `./packages/${name}` });
-    console.log(`Package "${name}" has been built successfully in:`, diff(timestamp), "seconds");
-  } catch (error) {
-    console.error(`Package: ${name} can not be built.`, String(error));
-    process.exit(1);
+const mapDependenciesFromPackageJson = (packageJson: Record<PropertyKey, unknown>): Target[] => {
+  if (!isObject(packageJson.dependencies)) {
+    return [];
   }
+
+  return Object.keys(packageJson.dependencies)
+    .filter((key) => key.startsWith("@monobox"))
+    .map((key) => key.split("/")[1]);
 };
 
-const run = async (name?: string): Promise<void> => {
-  if (isTarget(name)) {
-    return await build(name);
+const getPackages = async (): Promise<Set<Target>> => {
+  const packages = new Set<Target>();
+
+  const readTarget = async (target: Target): Promise<void> => {
+    const packageJson = await readFile(`./packages/${target}/package.json`, "utf8").then(JSON.parse);
+
+    if (!packageJson) {
+      return;
+    }
+
+    for (const target of mapDependenciesFromPackageJson(packageJson)) {
+      await readTarget(target);
+    }
+
+    packages.add(target);
+  };
+
+  for (const target of await readdir("./packages")) {
+    await readTarget(target);
+  }
+
+  return packages;
+};
+
+(async (arg?: string) => {
+  const packages = await getPackages();
+
+  const isTarget = (name?: string): name is Target => {
+    if (!name) {
+      return false;
+    }
+    if (packages.has(name)) {
+      return true;
+    }
+
+    console.error(`Package: ${name} can not be found.`);
+    process.exit(1);
+  };
+
+  const build = async (name: Target) => {
+    const timestamp = performance.now();
+
+    try {
+      await promisify(exec)("yarn build", { cwd: `./packages/${name}` });
+      console.log(`Package "${name}" has been built successfully in:`, diff(timestamp), "seconds");
+    } catch (error) {
+      console.error(`Package: ${name} can not be built.`, String(error));
+      process.exit(1);
+    }
+  };
+
+  if (isTarget(arg)) {
+    return await build(arg);
   }
 
   const timestamp = performance.now();
 
-  for (const name of Object.values(Targets)) {
+  for (const name of Array.from(packages)) {
     await build(name);
   }
 
   console.log("Total elapsed time:", diff(timestamp), "seconds");
-};
-
-run(process.argv[2]);
+})(process.argv[2]);
